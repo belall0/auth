5a0094ba0b3be8d9a276ba7a1a17a67f3808f7a6
Belal Muhammad Abu El-Ela <87620322+belall0@users.noreply.github.com>
2025-02-20 14:37:45 +0200

    feat: implement user signup functionality with email validation and password hashing

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import { loginSchema, signupSchema } from '@/lib/schema';
+import db from './db';
+import bcrypt from 'bcryptjs';
 
 type LoginValues = z.infer<typeof loginSchema>;
 type SignupValues = z.infer<typeof signupSchema>;
@@ -28,9 +30,10 @@ const login = async (values: LoginValues) => {
 };
 
 const signupAction = async (values: SignupValues) => {
+  // 1. validate the data
   const parsedData = signupSchema.safeParse(values);
 
-  await new Promise((resolve) => setTimeout(resolve, 1500));
+  // 2. check if the data is valid
   if (!parsedData.success) {
     return {
       success: false,
@@ -40,6 +43,36 @@ const signupAction = async (values: SignupValues) => {
     };
   }
 
+  // 3. check if the email is already taken or not
+  const user = await db.user.findUnique({
+    where: {
+      email: parsedData.data.email,
+    },
+  });
+
+  if (user) {
+    return {
+      success: false,
+      error: {
+        message: 'The email is already taken.',
+      },
+    };
+  }
+
+  // 4. construct the user object
+  const { name, email, password } = parsedData.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // 5. save the user to the database
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // 6. return the success message
   return {
     success: true,
     data: {
